36b9f82f8ec57b44b8408d2ff7d70e7b13d9c28d
129Ka <85550085+129Ka@users.noreply.github.com>
2021-06-08 20:07:06 +0800

    Update jddj_fruit.js

--- jddj/jddj_fruit.js
+++ jddj/jddj_fruit.js
@@ -17,6 +17,7 @@ cron "10 0,3,8,11,17 * * *" script-path=https://raw.githubusercontent.com/passer
 
 let isNotify = true;//是否通知,仅限nodejs
 let ckPath = './jddj_cookie.js';//ck路径,环境变量:JDDJ_CKPATH
+/*
 let cookies = []
 
   if (process.env.JDDJ_CKPATH && process.env.JDDJ_CKPATH.indexOf(',') > -1) {
@@ -28,7 +29,7 @@ let cookies = []
   
     console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
     console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
-
+*/
 const $ = new API("jddj_fruit");
 try {
     //https://cdn.jsdelivr.net/gh/passerby-b/JDDJ@main/jddj_fruit_code.js
